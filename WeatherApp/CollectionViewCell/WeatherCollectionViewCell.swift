8965b6bf3e50fef5c33b23ebae5e3c29959e0e62
ah_abdelhak <ah_abdelhak@yahoo.com>
2022-04-14 03:34:15 +0200

    init commit

---
+++ WeatherApp/CollectionViewCell/WeatherCollectionViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherCollectionViewCell.swift
+//  WeatherApp
+//
+//  Created by Ahmed Amer on 12/04/2022.
+//
+
+import UIKit
+
+class WeatherCollectionViewCell: UICollectionViewCell {
+
+    static let identifier = "WeatherCollectionViewCell"
+
+        static func nib() -> UINib {
+            return UINib(nibName: "WeatherCollectionViewCell",
+                         bundle: nil)
+        }
+
+        @IBOutlet var iconImageView: UIImageView!
+        @IBOutlet var tempLabel: UILabel!
+
+        func configure(with model: HourlyWeatherEntry) {
+            self.tempLabel.text = "\(model.temperature)"
+            self.iconImageView.contentMode = .scaleAspectFit
+            self.iconImageView.image = UIImage(named: "clear")
+        }
+
+        override func awakeFromNib() {
+            super.awakeFromNib()
+        }
+
+
+}
